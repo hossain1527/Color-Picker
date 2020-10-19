4dacb2a5f6650c6da86b728c73e3fcaa3a5d1ffc
hossain1527 <hossain.sci2015@gmail.com>
2020-10-19 19:53:04 +0100

    Updates

---
+++ precache-manifest.96be1f610431fe968e4e9858ee8ed9eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ce56028bdb5c85b078774421094548b",
+    "url": "/Color-Picker/index.html"
+  },
+  {
+    "revision": "54e726febab88d476d44",
+    "url": "/Color-Picker/static/css/main.097a0ce8.chunk.css"
+  },
+  {
+    "revision": "cddad98dc37a1a40e2a3",
+    "url": "/Color-Picker/static/js/2.3cde75d2.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Color-Picker/static/js/2.3cde75d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54e726febab88d476d44",
+    "url": "/Color-Picker/static/js/main.b0d3a121.chunk.js"
+  },
+  {
+    "revision": "aefa996b410b5d1feced",
+    "url": "/Color-Picker/static/js/runtime-main.f943ed82.js"
+  }
+]);
